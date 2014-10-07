9ee6ff293323477dd63930b8c41da7fec3252377
James Buckley <james.buckley@ericsson.com>
2014-10-07 15:04:19 +0100

    Updated UI of main and title activities

--- Flummoxed/gen/com/buckley/flummoxed/R.java
+++ Flummoxed/gen/com/buckley/flummoxed/R.java
@@ -1091,16 +1091,20 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
+        public static final int action_settings=0x7f050042;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int buttonPlay=0x7f05003e;
+        public static final int buttonSettings=0x7f050041;
+        public static final int buttonTutorial=0x7f050040;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
+        public static final int editText1=0x7f05003c;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
@@ -1110,6 +1114,7 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imageView1=0x7f05003f;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1119,6 +1124,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int scrollView1=0x7f05003d;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1201,6 +1207,7 @@ containing a value of this type.
         public static final int title=0x7f0c0001;
     }
     public static final class string {
+        public static final int FlummoxedLogo=0x7f0a0014;
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
          */
         public static final int abc_action_bar_home_description=0x7f0a0001;
@@ -1243,6 +1250,11 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        /**  Button Strings 
+         */
+        public static final int button_MainActivity=0x7f0a0011;
+        public static final int button_SettingsActivity=0x7f0a0012;
+        public static final int button_TutorialActivity=0x7f0a0013;
         public static final int hello_world=0x7f0a000e;
         public static final int title_activity_main=0x7f0a0010;
     }
